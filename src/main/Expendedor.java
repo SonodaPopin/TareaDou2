953c32146389b2da8ea3adb3be42c5561e3d037e
MalyZyndra <hflores2022@udec.cl>
2023-10-19 17:27:17 -0300

    Cambios: Expendedor

--- src/main/Expendedor.java
+++ src/main/Expendedor.java
@@ -1,13 +1,17 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Expendedor{
-    Deposito<Moneda> vuelto = new Deposito<>();
+    Deposito<Moneda> monedas = new Deposito<>();
     Deposito<CocaCola> cocas = new Deposito<>();
     Deposito<Sprite> sprites = new Deposito<>();
     Deposito<Fanta> fantas = new Deposito<>();
     Deposito<Super8> superochos = new Deposito<>();
     Deposito<Snicker> snickers = new Deposito<>();
-    public Expendedor(int numProductos){        
+    private ArrayList<Deposito<? extends Producto>> stock;
+    public Expendedor(int numProductos){
+        stock = new ArrayList<>();
         for(int i = 0; i < numProductos; i++){
             cocas.addT(new CocaCola(i));
             sprites.addT(new Sprite(i));
@@ -15,11 +19,20 @@ public class Expendedor{
             superochos.addT(new Super8(i));
             snickers.addT(new Snicker(i));
         }
+        stock.add(cocas);
+        stock.add(sprites);
+        stock.add(fantas);
+        stock.add(superochos);
+        stock.add(snickers);
     }   
     public Producto comprarProducto(Moneda m, int cual) {
     	int vuelto = m.getValor() - Inventario.getPrecio(cual);
+    	for (int i=0; i<vuelto/100; i++) {
+    		monedas.addT(new Moneda100());
+    	}
+    	return stock.get(cual).getT();
     }
     public Moneda getVuelto(){
-    	return vuelto.getT();
+    	return monedas.getT();
     }
 }
